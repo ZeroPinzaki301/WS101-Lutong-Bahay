5a629d2254f9f0719235a6ce931dac0f1dd43da8
Andrei T. <138087405+2004drei@users.noreply.github.com>
2025-03-13 17:02:41 +0800

    About Us
    Added contents for the About Us page, added the about-us.js and edited the sass file

---
+++ about-us.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.profile-carousel').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 30000
+    });
+  });
